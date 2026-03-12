1a4b1651ae175f76c454c5002f9e59d89de5263a
Giselle Martinez-Sanchez <77756063+GiselleMtnezS@users.noreply.github.com>
2026-03-12 15:28:51 -0500

    debugging deployment v2

--- lib/auth0.ts
+++ lib/auth0.ts
@@ -6,10 +6,10 @@ import { Auth0Client } from '@auth0/nextjs-auth0/server'
  * APP_BASE_URL is set automatically for Vercel preview deployments
  * via next.config.ts. For local dev, set it to http://localhost:3000.
  */
-console.log('AUTH0_BASE_URL', process.env.AUTH0_BASE_URL)
+console.log('APP_BASE_URL', process.env.APP_BASE_URL)
 export const auth0 = new Auth0Client({
   authorizationParameters: {
-    appBaseUrl: process.env.AUTH0_BASE_URL, // https://gmtnezschez.com/linky
+    appBaseUrl: process.env.APP_BASE_URL, // https://gmtnezschez.com/linky
     // Uncomment and set AUTH0_AUDIENCE in .env.local if you're using
     // Auth0 as an authorization server for your own API.
     audience: process.env.AUTH0_AUDIENCE,
