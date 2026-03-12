f7b8192f3af45899ca8e34e0da53130f11355309
Giselle Martinez-Sanchez <77756063+GiselleMtnezS@users.noreply.github.com>
2026-03-12 15:38:32 -0500

    debugging deployment v4

--- lib/auth0.ts
+++ lib/auth0.ts
@@ -7,6 +7,7 @@ import { Auth0Client } from '@auth0/nextjs-auth0/server'
  * via next.config.ts. For local dev, set it to http://localhost:3000.
  */
 console.log('AUTH0_BASE_URL', process.env.AUTH0_BASE_URL)
+console.log('APP_BASE_URL', process.env.APP_BASE_URL)
 export const auth0 = new Auth0Client({
   authorizationParameters: {
     appBaseUrl: process.env.AUTH0_BASE_URL, // https://gmtnezschez.com/linky
