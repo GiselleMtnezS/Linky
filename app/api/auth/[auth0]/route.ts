bcf2744ed1686b5bc2ffbe0c79a6878f65c3472f
Giselle Martinez-Sanchez <77756063+GiselleMtnezS@users.noreply.github.com>
2026-03-12 14:15:07 -0500

    auth route patch

--- app/api/auth/[auth0]/route.ts
+++ app/api/auth/[auth0]/route.ts
@@ -1,3 +1,4 @@
-import { handleAuth } from '@auth0/nextjs-auth0'
+import { auth0 } from '@/lib/auth0'
 
-export const GET = handleAuth()
+export const GET = auth0.middleware
+export const POST = auth0.middleware
